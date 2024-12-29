342fa84a757a17c6e5c2f8f637ece7d486652717
Muhammad Muneeb <muneebsaeed190@gamil.com>
2024-12-29 10:33:41 -0500

    Change the width and height of main and other parents

--- app.js
+++ app.js
@@ -10,3 +10,37 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+const mouse = document.getElementById('mouse');
+const para = document.querySelector('.para');
+const mouseHover = document.getElementById('mouse-hover');
+
+// Hide #mouse initially
+mouse.style.display = 'none';
+
+// Show #mouse and enable movement on hover
+para.addEventListener('mouseover', () => {
+    mouse.style.display = 'flex';
+});
+
+// Move #mouse with cursor inside #mouse-hover
+
+const offsetX = 65;
+const offsetY = 10;
+
+mouseHover.addEventListener("mousemove", function (e) {
+    const rect = mouseHover.getBoundingClientRect(); 
+    const x = e.clientX + offsetY - rect.left - mouse.offsetWidth / 2; 
+    const y = e.clientY + offsetX - rect.top - mouse.offsetHeight / 2; 
+
+    mouse.style.transform = `translate(${x}px, ${y}px)`; 
+    para.style.textDecoration = 'underLine'
+});
+
+
+// Hide #mouse when not hovering
+mouseHover.addEventListener('mouseleave', () => {
+    mouse.style.display = 'none';
+    para.style.textDecoration = 'none'
+});
